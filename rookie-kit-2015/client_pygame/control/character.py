d17ce1ea3f9d9ac1b6dd509523a448513e879cba
Kingame <chasulas912@gmail.com>
2015-11-14 02:37:36 -0700

    cool beans

--- rookie-kit-2015/client_pygame/control/character.py
+++ rookie-kit-2015/client_pygame/control/character.py
@@ -1,17 +1,17 @@
-import pygame
-import random
-from spritesheet import Spritesheet
+# import pygame
+# import random
+# from spritesheet import Spritesheet
 
 
-class Character:
-	def __init__(self, name, filename, sounds, rowLength):
-		self.name = name
-		self.rowL = rowLength
+# class Character:
+# 	def __init__(self, name, filename, sounds, rowLength):
+# 		self.name = name
+# 		self.rowL = rowLength
 		
 
 
-	def load_sprites(self):
-		self.lines = []
-		for s in self.rowL:
-			rect = pygame.Rect((0,s*(SPRITE_GUTTER+LINK_SPRITE),LINK_SPRITE,LINK_SPRITE))
-	        lines.append(sheet.load_irregular_strip(rect,LINK_SPRITE+SPRITE_GUTTER, LINK_STATE[s], (255,255,255)))
+# 	def load_sprites(self):
+# 		self.lines = []
+# 		for s in self.rowL:
+# 			rect = pygame.Rect((0,s*(SPRITE_GUTTER+LINK_SPRITE),LINK_SPRITE,LINK_SPRITE))
+# 	        lines.append(sheet.load_irregular_strip(rect,LINK_SPRITE+SPRITE_GUTTER, LINK_STATE[s], (255,255,255)))
